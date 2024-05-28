d330d99b101948881f3c03f9edac8cbc46dcd71f
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-05-28 16:47:19 -0300

    feat: Update ColorfulTitle component to accept JSX.Element as text prop

--- components/SocialMedias/index.tsx
+++ components/SocialMedias/index.tsx
@@ -4,24 +4,16 @@ import { Josefin_Slab } from "next/font/google";
 import { useState } from "react";
 import { socialMedias } from "@/data/social-medias";
 
-const JosefinSlab = Josefin_Slab({ subsets: ["latin"] });
+// const JosefinSlab = Josefin_Slab({ subsets: ["latin"] });
 
 const SocialMedias = () => {
-  const [itemHovered, setItemHovered] = useState<string | null>("");
-
   return (
     <footer
-      className={`${JosefinSlab.className} flex justify-center items-center gap-8 mb-5 md:mb-0 text-white w-full`}
+      className={`flex justify-center  py-3 items-center gap-8 mb-5 text-white w-full`}
     >
       {socialMedias.map((socialMedia) => (
         <div className="flex items-center gap-2" key={socialMedia.name}>
-          <a
-            href={socialMedia.url}
-            target="_blank"
-            onMouseEnter={() => setItemHovered(socialMedia.name)}
-            onMouseLeave={() => setItemHovered(null)}
-            rel="noopener noreferrer"
-          >
+          <a href={socialMedia.url} target="_blank" rel="noopener noreferrer">
             {socialMedia.icon}
           </a>
         </div>
