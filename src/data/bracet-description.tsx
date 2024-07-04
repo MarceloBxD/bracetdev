77a74d5bb4bf1190228fbade40d5b6bc879a0058
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-07-03 23:10:37 -0300

    ```text refactor: Update bracet description with additional technologies
    The code changes update the bracet description in the BRACET_DATA object to include additional technologies such as React Native and SCSS. This provides a more comprehensive overview of the technologies used by Marcelo Bracet.

--- src/data/bracet-description.tsx
+++ src/data/bracet-description.tsx
@@ -1,5 +1,6 @@
 export const BRACET_DATA = {
   name: "Marcelo Bracet",
   role: "Desenvolvedor Front End Júnior",
-  description: "NextJS & ReactJS | TypeScript | NodeJS | TailwindCSS",
+  description:
+    "NextJS & ReactJS | React Native | TypeScript | NodeJS | TailwindCSS | SCSS",
 };
